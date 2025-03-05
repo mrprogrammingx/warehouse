edaa092a7bbff5e6a52e7787138f00f1b9433161
amir <amirjoun2@gmail.com>
2025-03-05 20:23:41 +0400

    Added for showing the chart on dashboard(needs to implement)

---
+++ app/Http/Controllers/Api/MainDashboard/FoodController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\MainDashboard;
+
+use App\Http\Controllers\Controller;
+use App\Services\Globals\ResponsesService;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    public function getCountOfDeliveryReservationPerDayOfWeek()
+    {
+        //TODO
+        $result = ResponsesService::success([
+            'count' => [
+                ['count' => 1, 'date' => now()],
+                ['count' => 2, 'date' => now()],
+                ['count' => 3, 'date' => now()]
+            ]
+        ]);
+        
+        return response()->json($result, $result['status']);
+    }
+
+    public function getCountOfReservationPerDayOfWeek()
+    {
+        //TODO
+        $result = ResponsesService::success([
+            'count' => [
+                ['count' => 2, 'date' => now()],
+                ['count' => 3, 'date' => now()],
+                ['count' => 4, 'date' => now()]
+            ]
+        ]);        
+        
+        return response()->json($result, $result['status']);
+    }
+}
